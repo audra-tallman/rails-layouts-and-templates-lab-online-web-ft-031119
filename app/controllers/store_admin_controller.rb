65c04448561f39ed27af2bef0467d1144c1b95e8
Audra Tallman <audi.tallman@gmail.com>
2019-06-03 00:06:28 +0000

    Automatically backed up by Learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,2 +1,15 @@
 class StoreAdminController<ApplicaitonController
+  
+  layout 'admin'
+  def home
+  end
+
+  def orders
+    render :layout => "order_administration"
+  end
+
+  def invoice
+    render :layout => false
+  end
+  
 end 
